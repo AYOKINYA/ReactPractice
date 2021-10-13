3c7e4f5330282fa0aa7a988a03b79b744f687376
AYOKINYA <ayokinya@gmail.com>
2021-10-13 17:01:48 +0900

    [ADD] DoughnutLabel for AvgReceiptTime

--- src/components/demo/AvgReceiptTime.jsx
+++ src/components/demo/AvgReceiptTime.jsx
@@ -2,7 +2,9 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Doughnut } from 'react-chartjs-2';
 
-const toggleLegend = true;
+import drawDoughnutLabel from "./DoughnutLabel/core"
+
+const toggleLegend = false;
 
 const options = {
     scales: {
@@ -22,6 +24,11 @@ const options = {
     },
 };
 
+const doughnutLabel = {
+    id: 'doughnutLabel',
+    afterDraw: (chart) => drawDoughnutLabel(chart)
+};
+
 const AvgReceiptTime = () => {
 
     const [chartData, setChartData] = useState({});
@@ -32,22 +39,34 @@ const AvgReceiptTime = () => {
         .get("https://7ad89e8d-fea5-4572-a58d-82c6d3f07aa2.mock.pstmn.io/dashboard/receipttime")
         .then(
             (res) => {
+                    const mean = Math.floor((res.data.GoodsReleaseMaxTime + res.data.GoodsReleaseMinTime) / 2);
                     setChartData({
                         datasets: [{
-                            data: Object.values(res.data),
+                            data: [mean, 100 - mean], // 100 is a tentative number
                             backgroundColor: [
-                                "#77CEFF",
-                                "#0079AF",
-                            ]
+                                '#3ec556',
+                                'rgba(0, 0, 0, 0)'
+                            ],
+                            hoverBackgroundColor: [
+                                '#3ec556',
+                                'rgba(0, 0, 0, 0)'
+                            ],
+                            borderColor: [
+                                '#3ec556',
+                                'rgba(0, 0, 0, 0)'
+                            ],
+                            rotation: 200,
+                            cutout: '70%',
+                            borderRadius: 120
                         }]
-                    })
+                    });
                 }
             );
     }, []);
 
     return (
         <div>
-            <Doughnut data={chartData} options={options} />
+            <Doughnut data={chartData} options={options} plugins={[doughnutLabel]} />
         </div>
     )
 }
